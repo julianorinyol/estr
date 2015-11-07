060e2ba253b8af1a26b452cf060a849615ca296f
JulianOrinyol <julianorinyol@gmail.com>
2015-11-07 15:37:11 -0800

    added strong params method for controllers

--- app/controllers/client_profile_controller.rb
+++ app/controllers/client_profile_controller.rb
@@ -1,2 +1,10 @@
 class ClientProfileController < ApplicationController
+  def create
+    @client_profile = ClientProfile.new(client_profile_params)
+  end
+
+  def subscriber_params
+    # params.require(:client_profile).permit(:first_name, :last_name, user_attributes: [ :email, :password, :password_confirmation, :address, :id ])
+    params.require(:client_profile).permit!
+  end
 end
--- app/controllers/manager_profile_controller.rb
+++ app/controllers/manager_profile_controller.rb
@@ -1,2 +1,11 @@
 class ManagerProfileController < ApplicationController
+  def create
+    @manager_profile = ManagerProfile.new(manager_profile_params)
+  end
+
+  def subscriber_params
+    # params.require(:manager_profile).permit(:name, user_attributes: [ :email, :password, :password_confirmation, :address, :id ])
+    params.require(:manager_profile).permit!
+  end
+
 end
