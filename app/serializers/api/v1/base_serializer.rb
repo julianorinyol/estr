761f65c04273b94d61960348beb30dab6d3e97d9
jsivakumaran <jsivakumaran@outlook.com>
2015-11-08 17:56:51 -0800

    Working users API call for error404 - not found

--- app/serializers/api/v1/base_serializer.rb
+++ app/serializers/api/v1/base_serializer.rb
@@ -1,7 +1,4 @@
-class Api::V1::UserSerializer < Api::V1::BaseSerializer
-  attributes :id, :email, :name, :role, :created_at, :updated_at
-  has_and_belongs_to_many :AppointmentsUsers
-
+class Api::V1::BaseSerializer < ActiveModel::Serializer
   def created_at
     object.created_at.in_time_zone.iso8601 if object.created_at
   end
