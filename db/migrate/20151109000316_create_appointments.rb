192fd75e907d83a4daa4daac3df76433c9d5bbdc
jsivakumaran <jsivakumaran@outlook.com>
2015-11-08 16:27:26 -0800

    Add appointments migration

--- db/migrate/20151109000316_create_appointments.rb
+++ db/migrate/20151109000316_create_appointments.rb
@@ -1,7 +1,10 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-
+      t.belongs_to :user
+      t.belongs_to :client
+      t.datetime :appointment_date
+      t.boolean :finished, default: false
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,12 @@
 ActiveRecord::Schema.define(version: 20151109000316) do
 
   create_table "appointments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "client_id"
+    t.datetime "appointment_date"
+    t.boolean  "finished",         default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
   end
 
   create_table "users", force: :cascade do |t|
